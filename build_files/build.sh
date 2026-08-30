598e0c1f0a6cae9cc88ddd709a2714488738c548
kodiak23 <kyle@kodiak23.dev>
2026-08-29 22:46:02 -0400

    worked on adding list of essential system packages to install on base fedora coreos

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,41 +5,33 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# install packages for homebrew
+# Install development-tools for homebrew
 dnf group -y install development-tools
-dnf -y install procps-ng curl file
+dnf -y install procps-ng
 
-# install packages
+# Install packages
 dnf -y install \
-  bootc \
   docker-buildx \
   docker-compose \
-  podman \
-  podman-compose \
-  qemu-guest-agent \
-  tmux \
+  ceph-common \
+  git-lfs \
   man-db \
-  pciutils \
+  podman-compose \
+  procps-ng \
+  qemu-guest-agent \
+  stow \
+  uutils-coreutils \
+  wget \
   ufw
-# firewalld
-# fwupd-efi
-# open-vm-tools
-# pv
+# btop
+# zoxide
+# pciutils # Don't really need this on the base vm image without pci-passthrough devices
+# fwupd-efi # Don't really need this on the base vm image
 
-# Use a COPR Example:
-#
+# Enable COPR for package install then disable immediately so they don't end up enabled on the final image:
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
-
+# Enable/Disable system unit files
 # systemctl enable podman.socket
