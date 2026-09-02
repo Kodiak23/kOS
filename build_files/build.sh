4d0706551ab92926063dcd5ca63c766b5f8bd2c6
kodiak23 <kyle@kodiak23.dev>
2026-09-01 21:38:22 -0400

    finished first draft of build.sh. Decided to run firewalld instead of ufw, because I checked on a fedora image and ufw uses legacy dependencies, firewalld just seems to be more modern. added fish here so its integrated better into the os instead of installing it with homebrew. going to try homebrew without development-tools first

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,23 +5,25 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-# Install development-tools for homebrew
-dnf group -y install development-tools
-
 # Install packages
 dnf -y install \
   docker-buildx \
   docker-compose \
-  ceph-common \
+  firewalld \
+  fish \
   git-lfs \
   man-db \
   podman-compose \
-  procps-ng \
   qemu-guest-agent \
   uutils-coreutils \
-  wget \
-  ufw
+  wget
+
+# ceph-common # about 220MB to install, and I don't need this on every server? maybe. I might make a different image tag for it. Undecided
 # pciutils # Don't need this on the base vm image until an image needs pci-passthrough devices
+
+# Development-tools for homebrew, but the group of packages is large, going to see if I can do without first
+# dnf group -y install development-tools
+# dnf -y install procps-ng
 
 # Enable COPR for package install then disable immediately so they don't end up enabled on the final image:
 # dnf5 -y copr enable ublue-os/staging
